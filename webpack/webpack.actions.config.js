59e390506714504ab2bc162ba770919cd937792a
Ruslan Mirkhusainov <v-rmirkhusainov@expediagroup.com>
2024-02-01 12:29:33 +0100

    getting secret from actions

---
+++ webpack/webpack.actions.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const glob = require('glob')
+const webpack = require('webpack');
+
+const backgroundFiles = glob.sync(path.join(__dirname, '../src/background/**/*.ts*'))
+const contentFiles = glob.sync(path.join(__dirname, '../src/content/**/*.ts*'))
+
+console.log("content files: ", contentFiles)
+console.log("background files: ", backgroundFiles)
+
+module.exports = {
+    mode: "production",
+    entry: {
+        background: backgroundFiles,
+        content: contentFiles
+    },
+    optimization: {
+        minimize: false
+    },
+    output: {
+        path: path.join(__dirname, "../dist"),
+        filename: "[name].js",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: ".", to: ".", context: "public" }]
+        }),
+        new webpack.DefinePlugin({
+            API_KEY: JSON.stringify(process.env.API_KEY)
+        })
+    ],
+};
